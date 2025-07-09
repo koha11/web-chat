1a1cf8b22e953b38ab1f8d6d7826eb2ed76839d5
koha11 <khoa88108@gmail.com>
2025-07-09 23:41:48 +0700

    add demo user camera display

--- web-chat-app/src/pages/call/Call.tsx
+++ web-chat-app/src/pages/call/Call.tsx
@@ -13,7 +13,13 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from "../../components/ui/collapsible";
-import { useRef, useState } from "react";
+import {
+  startTransition,
+  useEffect,
+  useRef,
+  useState,
+  VideoHTMLAttributes,
+} from "react";
 
 const Call = () => {
   const [isVideoCollapsibleOpen, setVideoCollapsibleOpen] = useState(false);
@@ -22,56 +28,60 @@ const Call = () => {
     return await navigator.mediaDevices.getUserMedia(constraints);
   };
 
-  const displayMedia = async (constraints: DisplayMediaStreamOptions) => {
-    return await navigator.mediaDevices.getDisplayMedia(constraints);
-  };
+  const myRef = useRef<HTMLVideoElement>(null);
 
-  const stream = openMediaDevices({ video: true, audio: true });
-  const myRef = useRef
+  useEffect(() => {
+    if (isVideoCollapsibleOpen) {
+      const stream = openMediaDevices({ video: true, audio: true });
 
-  stream.then((test) => {
-    return (
-      <div className="relative w-full h-screen flex justify-center items-center">
-        <div className="flex items-center justify-center gap-4 absolute top-4">
-          Top
-        </div>
-        <div className="flex flex-col justify-center items-center gap-2">
-          <div
-            className={`w-12 h-12 rounded-full bg-contain bg-no-repeat bg-center`}
-            style={{ backgroundImage: `url(/assets/images/google-logo.png)` }}
-          ></div>
-          <div className="text-xl font-bold">Name</div>
-          <div className="text-sm">Is calling...</div>
-        </div>
-        <div className="flex items-center justify-center gap-4 absolute bottom-4">
-          <Button className="rounded-full cursor-pointer">
-            <ScreenShare></ScreenShare>
-          </Button>
-          <Button className="rounded-full cursor-pointer">
-            <UserPlus2Icon></UserPlus2Icon>
-          </Button>
-          <Button className="rounded-full cursor-pointer">
-            <Video></Video>
-          </Button>
-          <Button className="rounded-full cursor-pointer">
-            <Mic></Mic>
-          </Button>
-          <Button className="rounded-full cursor-pointer bg-red-600 hover:bg-red-500">
-            <PhoneIcon></PhoneIcon>
-          </Button>
-        </div>
-        <Collapsible
-          className="absolute bottom-4 right-4 h-[25%] w-[20%]"
-          open={isVideoCollapsibleOpen}
-          onOpenChange={setVideoCollapsibleOpen}
+      stream.then((myStream) => {
+        myRef.current!.srcObject = myStream;
+      });
+    }
+  }, [isVideoCollapsibleOpen]);
+
+  return (
+    <div className="relative w-full h-screen flex justify-center items-center">
+      <div className="flex items-center justify-center gap-4 absolute top-4">
+        Top
+      </div>
+      <div className="flex flex-col justify-center items-center gap-2">
+        <div
+          className={`w-12 h-12 rounded-full bg-contain bg-no-repeat bg-center`}
+          style={{ backgroundImage: `url(/assets/images/google-logo.png)` }}
+        ></div>
+        <div className="text-xl font-bold">Name</div>
+        <div className="text-sm">Is calling...</div>
+      </div>
+      <div className="flex items-center justify-center gap-4 absolute bottom-4">
+        <Button className="rounded-full cursor-pointer">
+          <ScreenShare></ScreenShare>
+        </Button>
+        <Button className="rounded-full cursor-pointer">
+          <UserPlus2Icon></UserPlus2Icon>
+        </Button>
+        <Button className="rounded-full cursor-pointer">
+          <Video></Video>
+        </Button>
+        <Button className="rounded-full cursor-pointer">
+          <Mic></Mic>
+        </Button>
+        <Button className="rounded-full cursor-pointer bg-red-600 hover:bg-red-500">
+          <PhoneIcon></PhoneIcon>
+        </Button>
+      </div>
+      <Collapsible
+        className="absolute bottom-4 right-4 h-[25%] w-[20%]"
+        open={isVideoCollapsibleOpen}
+        onOpenChange={setVideoCollapsibleOpen}
+      >
+        <CollapsibleTrigger
+          hidden={isVideoCollapsibleOpen}
+          className="bg-gray-200 h-full w-4 absolute bottom-4 right-0 rounded-l-2xl flex items-center justify-center"
         >
-          <CollapsibleTrigger
-            hidden={isVideoCollapsibleOpen}
-            className="bg-gray-200 h-full w-4 absolute bottom-4 right-0 rounded-l-2xl flex items-center justify-center"
-          >
-            <ArrowBigLeftDashIcon></ArrowBigLeftDashIcon>
-          </CollapsibleTrigger>
-          {/* <CollapsibleContent
+          <ArrowBigLeftDashIcon></ArrowBigLeftDashIcon>
+        </CollapsibleTrigger>
+        {/* <CollapsibleContent
           className="absolute bottom-4 right-4 h-full w-full bg-contain bg-no-repeat bg-center rounded-md bg-red-200"
           style={{ backgroundImage: `url(/assets/images/google-logo.png)` }}
           
@@ -80,23 +90,20 @@ const Call = () => {
             <ArrowBigRightDashIcon></ArrowBigRightDashIcon>
           </CollapsibleTrigger>
         </CollapsibleContent> */}
-          <CollapsibleContent>
-            <video
-              className="absolute bottom-4 right-4 h-full w-full rounded-md200"
-              autoPlay
-              playsInline
-              ref={t}
-            ></video>
-            <CollapsibleTrigger className="bg-gray-200 h-full w-4 absolute left-0 rounded-l-md flex items-center justify-center">
-              <ArrowBigRightDashIcon></ArrowBigRightDashIcon>
-            </CollapsibleTrigger>
-          </CollapsibleContent>
-        </Collapsible>
-      </div>
-    );
-  });
-
-  return <></>;
+        <CollapsibleContent>
+          <video
+            className="absolute bottom-4 right-4 h-full w-full rounded-lg scale-x-[-1]"
+            autoPlay
+            playsInline
+            ref={myRef}
+          ></video>
+          <CollapsibleTrigger className="bg-gray-200 h-full w-4 absolute bottom-4 left-0 rounded-l-md flex items-center justify-center">
+            <ArrowBigRightDashIcon></ArrowBigRightDashIcon>
+          </CollapsibleTrigger>
+        </CollapsibleContent>
+      </Collapsible>
+    </div>
+  );
 };
 
 export default Call;
