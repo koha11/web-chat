43975925047ee2625ea62ed48b53d82aa3d342e6
koha11 <khoa88108@gmail.com>
2025-08-16 14:51:18 +0700

    improve UX when change nickname

--- web-chat-app/src/components/chat/ChangeNicknamesDialog.tsx
+++ web-chat-app/src/components/chat/ChangeNicknamesDialog.tsx
@@ -27,6 +27,28 @@ const ChangeNicknamesDialog = ({
     setEditNickname("");
   };
 
+  const handleChangedNickname = async ({
+    e,
+    isEdit,
+    userId,
+  }: {
+    e: any;
+    isEdit: boolean;
+    userId: string;
+  }) => {
+    if (isEdit && changedNickname != "") {
+      e.stopPropagation();
+      await changeNickname({
+        variables: {
+          chatId: chat.id,
+          changedUserId: userId,
+          nickname: changedNickname,
+        },
+      });
+      reset();
+    }
+  };
+
   return (
     <Dialog
       open={isOpen}
@@ -41,46 +63,55 @@ const ChangeNicknamesDialog = ({
             Nicknames
           </DialogTitle>
         </DialogHeader>
+
         <div className="flex gap-6 flex-col py-2">
-          {(chat.users as IUser[]).map((user) => {
-            const isEdit = isEditNickname == user.id;
-            const hasNickname =
-              chat.usersInfo[user.id].nickname != user.fullname;
-            const nickname = chat.usersInfo[user.id].nickname;
+          {Object.keys(chat.usersInfo).map((userId) => {
+            const isEdit = isEditNickname == userId;
+
+            const { avatar, fullname, nickname } = chat.usersInfo[userId];
+
+            const hasNickname = nickname != fullname;
 
             return (
               <div
-                key={user.id}
+                key={userId}
                 className="flex justify-between items-center cursor-pointer rounded-md"
                 onClick={() => {
-                  setEditNickname(user.id);
-                  setChangedNickname(nickname);
+                  if (!isEdit) {
+                    setEditNickname(userId);
+                    setChangedNickname(nickname);
+                  }
                 }}
               >
                 <div className="flex items-center gap-4 w-full">
                   <div
                     className={`w-12 h-12 rounded-full bg-contain bg-no-repeat bg-center`}
-                    style={{ backgroundImage: `url(${user.avatar})` }}
+                    style={{ backgroundImage: `url(${avatar})` }}
                   ></div>
+
                   {isEdit ? (
                     <div className="text-sm w-[80%]">
                       <Input
                         className="w-full"
-                        placeholder={!hasNickname ? user.fullname : ""}
+                        placeholder={!hasNickname ? fullname : ""}
                         value={changedNickname}
                         autoFocus={true}
                         onChange={(e) => setChangedNickname(e.target.value)}
+                        onKeyDown={(e) => {
+                          if (e.key == "Enter")
+                            handleChangedNickname({ e, isEdit, userId });
+                        }}
                       ></Input>
                     </div>
                   ) : (
                     <div className="text-sm">
                       <div className="font-bold">{nickname}</div>
-                      <div>
-                        {hasNickname ? user.fullname : "Place nickname"}
-                      </div>
+                      <div>{hasNickname ? fullname : "Place nickname"}</div>
                     </div>
                   )}
                 </div>
+
+                {/* close btn */}
                 {isEdit && (
                   <Button
                     className="rounded-full bg-white text-black hover:bg-gray-300 cursor-pointer"
@@ -92,21 +123,11 @@ const ChangeNicknamesDialog = ({
                     <X></X>
                   </Button>
                 )}
+
+                {/* confirm btn  */}
                 <Button
                   className="rounded-full bg-white text-black hover:bg-gray-300 cursor-pointer"
-                  onClick={(e) => {
-                    if (isEdit && changedNickname != "") {
-                      e.stopPropagation();
-                      changeNickname({
-                        variables: {
-                          chatId: chat.id,
-                          changedUserId: user.id,
-                          nickname: changedNickname,
-                        },
-                      });
-                      reset();
-                    }
-                  }}
+                  onClick={(e) => handleChangedNickname({ e, isEdit, userId })}
                 >
                   {isEdit ? <Check></Check> : <Edit></Edit>}
                 </Button>
